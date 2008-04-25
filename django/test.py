fd4c2a1c5f47f4db05d575d541c0a83604315cb8
Philip Jägenstedt <philip@foolip.org>
2008-04-25 21:44:36 +0000

    cruft
    git-svn-id: file:///media/hdd0/svn/mushup@16 e7c1934a-894b-0410-b661-f963fa50a05f

--- django/test.py
+++
@@ -1,9 +0,0 @@
-import pyexpat
-
-from mod_python import apache
-
-def handler(req):
-    req.content_type = 'text/plain'
-    req.send_http_header()
-    req.write('with pyexpat\n')
-    return apache.OK
