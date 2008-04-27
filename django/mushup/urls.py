8d3b7d8337089ffa94bb43340506185aeb22f03f
Philip Jägenstedt <philip@foolip.org>
2008-04-27 08:21:14 +0000

    stuff
    git-svn-id: file:///media/hdd0/svn/mushup@22 e7c1934a-894b-0410-b661-f963fa50a05f

--- django/mushup/urls.py
+++ django/mushup/urls.py
@@ -4,7 +4,8 @@ urlpatterns = patterns('',
     # Example:
     (r'^$', 'mushup.views.index'),
     (r'^login/', include('mushup.login.urls')),
-    (r'^logout/', 'mushup.views.logout'),
+    (r'^logout/$', 'mushup.views.logout'),
+    (r'^search/([^/]*)/([^/]*)/$', 'mushup.search.views.search'),
 
     # Uncomment this for admin:
 #     (r'^admin/', include('django.contrib.admin.urls')),
