2f9d10ff1f51be6aa3968705e61da2f4f0f0fee8
Philip Jägenstedt <philip@foolip.org>
2008-04-28 14:04:40 +0000

    d00d
    git-svn-id: file:///media/hdd0/svn/mushup@35 e7c1934a-894b-0410-b661-f963fa50a05f

---
+++ neo4j/src/test/java/org/foolip/mushup/NeoBrainz.java
@@ -0,0 +1,36 @@
+package org.foolip.mushup;
+
+import org.neo4j.util.btree.*;
+import org.neo4j.api.core.*;
+import org.foolip.mushup.*;
+
+import java.util.UUID;
+
+public class NeoBrainz {
+
+    public enum RelationshipTypes  {
+	MUSICBRAINZ,
+	ARTISTS,
+	ARTIST
+    }
+
+    public static void main(String[] args) {
+        NeoService neo = new EmbeddedNeo("neobrainz");
+
+	Transaction tx = Transaction.begin();
+	try {
+	    MushArtistFactory maf = new MushArtistFactory(neo);
+
+	    MushArtist artist = maf.createArtist();
+	    artist.setId(UUID.randomUUID());
+	    artist.setName("The Beatles");
+	    artist.setSortName("Beatles, The");
+	    
+	    tx.success();
+	} finally {
+	    tx.finish();
+	    neo.shutdown();
+	}
+	System.out.println("OK");
+    }
+}
