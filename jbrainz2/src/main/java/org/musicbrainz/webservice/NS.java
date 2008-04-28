46ea0b9a298c36eaa978e5f45710714870e12efe
Philip Jägenstedt <philip@foolip.org>
2008-04-28 19:36:17 +0000

    wtf
    git-svn-id: file:///media/hdd0/svn/mushup@39 e7c1934a-894b-0410-b661-f963fa50a05f

---
+++ jbrainz2/src/main/java/org/musicbrainz/webservice/NS.java
@@ -0,0 +1,8 @@
+package org.musicbrainz.webservice;
+
+final class NS {
+    private NS() {}
+    static final String MMD_1 = "http://musicbrainz.org/ns/mmd-1.0#";
+    static final String REL_1 = "http://musicbrainz.org/ns/rel-1.0#";
+    static final String EXT_1 = "http://musicbrainz.org/ns/ext-1.0#";
+}
---
+++ jbrainz2/src/main/java/org/musicbrainz/webservice/ParserException.java
@@ -0,0 +1,7 @@
+package org.musicbrainz.wsxml;
+
+public class ParserException extends Exception {
+    public ParserException() {}
+    public ParserException(String message) {super(message);}
+    public ParserException(Throwable cause) {super(cause);}
+}
