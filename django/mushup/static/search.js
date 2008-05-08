5259092f49b321d0ab9552c98c3541e606fb8b80
Philip Jägenstedt <philip@foolip.org>
2008-05-08 13:26:48 +0000

    bla
    git-svn-id: file:///media/hdd0/svn/mushup@55 e7c1934a-894b-0410-b661-f963fa50a05f

--- django/mushup/static/search.js
+++ django/mushup/static/search.js
@@ -1,9 +1,30 @@
 function initSearch() {
-    YAHOO.util.Event.addListener("searchform", "submit", search);
+    YAHOO.util.Event.addListener("search", "submit", search);
 }
 
 function responseSuccess(o) {
-    
+    try {
+	var artists = YAHOO.lang.JSON.parse(unescape(o.responseText));
+    }
+    catch (e) {
+	alert(unescape(o.responseText));
+	return;
+    }
+
+    var c = document.getElementById("content");
+    while (c.hasChildNodes()) {
+	c.removeChild(c.firstChild);
+    }
+
+    for (var i=0; i<artists.length; i++) {
+	var artist = artists[i];
+	var a = document.createElement("a");
+	a.setAttribute("href", "http://musicbrainz.org/artist/" + artist.id + ".html");
+	a.appendChild(document.createTextNode(artist.name));
+	var p = document.createElement("p");
+	p.appendChild(a)
+	c.appendChild(p);
+    }
 }
 
 function responseFailure(o) {
@@ -11,8 +32,12 @@ function responseFailure(o) {
 }
 
 function search(e) {
-    var artist = e.target.artistsearch.value;
-    var url = "/search/artist/" + encodeURI(artist);
+    var searchQuery = e.target.query.value;
+    var searchType = e.target.type.value;
+    var url = ROOT + "search/";
+    if (searchType != "all")
+	url += searchType + "/";
+    url += encodeURI(searchQuery) + "?format=json";
     var callback = {
 	success:responseSuccess,
 	failure:responseFailure,
