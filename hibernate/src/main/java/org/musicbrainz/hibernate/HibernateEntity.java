69e76ff830b0bded53c9d565c201711d305b9d7c
Philip Jägenstedt <philip@foolip.org>
2008-10-18 13:53:46 +0000

    support artist aliases
    git-svn-id: file:///media/hdd0/svn/mushup@73 e7c1934a-894b-0410-b661-f963fa50a05f

--- hibernate/src/main/java/org/musicbrainz/hibernate/HibernateEntity.java
+++ hibernate/src/main/java/org/musicbrainz/hibernate/HibernateEntity.java
@@ -5,50 +5,40 @@ import org.musicbrainz.model.*;
 import java.util.UUID;
 
 public class HibernateEntity implements Entity {
-	private int dbrow;
+	private int row;
 	private UUID mbid;
 
-    public HibernateEntity() {}
+	public HibernateEntity() {}
 
-    public int getRow() {
-	return this.dbrow;
-    }
+	public int getRow() {
+		return this.row;
+	}
 
-    public void setRow(int row) {
-	this.dbrow = row;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public String getUuid() {
-    	if (this.mbid != null)
-    		return this.mbid.toString();
-    	return null;
-    }
+	public UUID getId() {
+		return this.mbid;
+	}
 
-    public void setUuid(String id) {
-    	this.mbid = UUID.fromString(id);
-    }
-    
-    public UUID getId() {
-    	return this.mbid;
-    }
+	public void setId(UUID id) {
+		this.mbid = id;
+	}
 
-    public void setId(UUID id) {
-    	this.mbid = id;
-    }
-    
-    public UUID getXid() {
-    	return this.mbid;
-    }
+	public UUID getUuid() {
+		return getId();
+	}
 
-    public void setXid(UUID id) {
-    	this.mbid = id;
-    }
+	public void setUuid(UUID id) {
+		setId(id);
+	}
 
-    public Iterable<UrlRelation> getUrlRelations() {
-    	return null;
-    }
+	public Iterable<UrlRelation> getUrlRelations() {
+		return null;
+	}
 
-    public void addUrlRelation(UrlRelation urlRel) {
+	public void addUrlRelation(UrlRelation urlRel) {
 
-    }
+	}
 }
